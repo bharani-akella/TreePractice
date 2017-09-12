335dc98cb2182e75e6fcd38f8c43c79db36371cd
Bharani Akella <bharani0akella@gmail.com>
2017-09-12 18:05:04 +0530

    Add files via upload

---
+++ IdenticalTrees.java
@@ -0,0 +1,71 @@
+import java.util.Stack;
+
+public class IdenticalTrees {
+Node root1;
+Node root2;
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+IdenticalTrees obj=new IdenticalTrees();
+obj.root1=obj.insert(obj.root1,4);
+obj.root1=obj.insert(obj.root1,2);
+obj.root1=obj.insert(obj.root1,6);
+obj.root1=obj.insert(obj.root1,1);
+obj.root1=obj.insert(obj.root1,3);
+obj.root1=obj.insert(obj.root1,5);
+obj.root1=obj.insert(obj.root1,7);
+
+obj.root2=obj.insert(obj.root2,4);
+obj.root2=obj.insert(obj.root2,2);
+obj.root2=obj.insert(obj.root2,6);
+obj.root2=obj.insert(obj.root2,1);
+obj.root2=obj.insert(obj.root2,3);
+obj.root2=obj.insert(obj.root2,5);
+//obj.root2=obj.insert(obj.root2,7);
+
+boolean result=obj.identical(obj.root1, obj.root2);
+
+System.out.println(result);
+
+	}
+	
+	
+	public Node insert(Node root, int key) {
+		if(root==null){
+			return new Node(key);
+		} 
+		if(key<root.data){
+			root.left=insert(root.left, key);
+		}
+		
+		if(key>root.data){
+			root.right=insert(root.right, key);
+		}
+		
+		return root;
+	}
+	
+	public void inOrder(Node root){
+		if(root==null){
+			return;
+		}
+		
+		inOrder(root.left);
+		System.out.print(root.data+"->");
+		inOrder(root.right);
+	}
+	
+	public boolean identical(Node root1,Node root2){
+		if(root1==null && root2==null){
+			return true;
+		}
+		
+		if(root1!=null && root2!=null){
+			if((root1.data==root2.data)&&(identical(root1.left,root2.left))&&identical(root1.right,root2.right)){
+				return true;
+			}
+		}
+		return false;
+	}
+		
+}
